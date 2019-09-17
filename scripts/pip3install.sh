545399b102ff776200a90680b9ebeab3d0e4acfa
Recep Cetin <recep.cetin@outlook.com>
2019-09-17 17:02:54 +0200

    improve installed check - remove trailing whitespace

--- scripts/pip3install.sh
+++ scripts/pip3install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 function install {
-    QUERY=$(pip3 show ${1} | grep "Name:" | cut --delimiter=' ' -f 2)
-    echo $QUERY
+    QUERY=$(pip3 show ${1} | grep "Name:" | cut --delimiter=' ' -f 2 | xargs)
 
     shopt -s nocasematch # Make caseinsentive
     if [[ $QUERY == *"${1}"* ]]; then
