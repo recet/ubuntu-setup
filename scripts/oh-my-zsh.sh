2e1c013fb96b5fc4896022c6e54b2a3ba3a4cb3d
Recep Cetin <recep.cetin@outlook.com>
2019-10-19 23:10:15 +0200

    more robust oh-my-zsh install..

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -2,7 +2,9 @@
 
 if [ ! -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
     echo "git cloning oh-my-zsh.."
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    mkdir -p ~/tmp/oh-my-zsh
+    git clone  https://github.com/robbyrussell/oh-my-zsh.git ~/tmp/
+    mv ~/tmp/oh-my-zsh/* ~/.oh-my-zsh/*
 else
     echo "Already installed: ~/.oh-my-zsh exist... skipping"
 fi
