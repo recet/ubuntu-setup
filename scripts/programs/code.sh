07ce9b21c439b834554a4d68b1739158282ce866
Recep Cetin <recep.cetin@outlook.com>
2019-10-19 23:09:02 +0200

    make sure .config file is owned by current user

--- scripts/programs/code.sh
+++ scripts/programs/code.sh
@@ -3,7 +3,7 @@
 # Visual Studio Code
 # https://code.visualstudio.com/docs/setup/linux
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo install -o $USER -g $USER -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 sudo apt update
