bcb64f1d1ef827922d65522cf2d23329a33a0b90
Recep Cetin <recep.cetin@outlook.com>
2019-10-19 23:07:39 +0200

    add sudo atom install

--- scripts/programs/atom.sh
+++ scripts/programs/atom.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
 
