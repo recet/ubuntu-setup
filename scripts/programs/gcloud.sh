9f1239fd6bbd44e7e670ab710585e3c9e42ad79c
Recep Cetin <recep.cetin@outlook.com>
2019-10-19 14:59:44 +0200

    remove append on tee

--- scripts/programs/gcloud.sh
+++ scripts/programs/gcloud.sh
@@ -3,7 +3,7 @@
 # Instructions from https://cloud.google.com/sdk/docs/quickstart-debian-ubuntu
 
 # Add the Cloud SDK distribution URI as a package source
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 
 # Import the Google Cloud Platform public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
--- scripts/programs/kubectl.sh
+++ scripts/programs/kubectl.sh
@@ -3,6 +3,6 @@
 # Instructions from https://kubernetes.io/docs/tasks/tools/install-kubectl/
 sudo apt-get update
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
