7dd24fa577249c34ec4251627c1f4a15a8ec0cff
Recep Cetin <recep.cetin@outlook.com>
2019-10-21 10:38:03 +0200

    remove sudo from wget

--- scripts/programs/nordvpn.sh
+++ scripts/programs/nordvpn.sh
@@ -2,8 +2,8 @@
 
 # Instructions from https://support.nordvpn.com/Connectivity/Linux/1325531132/Installing-and-using-NordVPN-on-Debian-Ubuntu-and-Linux-Mint.htm
 
-sudo wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-sudo dpkg -i nordvpn-release_1.0.0_all.deb
+wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+dpkg -i nordvpn-release_1.0.0_all.deb
 
 sudo apt update
 sudo apt install nordvpn -y
