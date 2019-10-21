b47b4c903c5acf090cac34999bbd0c3891490bf2
Recep Cetin <recep.cetin@outlook.com>
2019-10-21 08:28:32 +0200

    add sudo

--- scripts/programs/helm.sh
+++ scripts/programs/helm.sh
@@ -3,7 +3,7 @@
 # Instructions from https://helm.sh/docs/using_helm/#from-script
 
 curl -LO https://git.io/get_helm.sh
-chmod 700 get_helm.sh
+sudo chmod 700 get_helm.sh
 ./get_helm.sh --version v2.13.1
 
 rm get_helm.sh
