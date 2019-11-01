5716be011151ad7960894e89ff9c9889618b5579
Recep Cetin <recep.cetin@outlook.com>
2019-11-01 12:13:37 +0100

    make linux use local time - fixes time issue on windows for dual boot

--- scripts/additional-os-settings.sh
+++ scripts/additional-os-settings.sh
@@ -2,3 +2,8 @@
 
 # Activate Night Light based on sunrise (true or false)
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+
+# Make Linux Use Local Time - fixing dual-boot windows showing wrong time
+# instructions from https://www.howtogeek.com/323390/how-to-fix-windows-and-linux-showing-different-times-when-dual-booting/
+# If you ever want to undo this change, run the following command: timedatectl set-local-rtc 0 --adjust-system-clock
+timedatectl set-local-rtc 1 --adjust-system-clock
