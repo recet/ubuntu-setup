3b510bada6a2d3711a76f4b07c6c87b3a7965136
Spikra <espen.blikra@outlook.com>
2019-11-05 14:05:01 +0100

    fixed npm dependencies and vordvpn install

--- scripts/programs/nordvpn.sh
+++ scripts/programs/nordvpn.sh
@@ -3,7 +3,7 @@
 # Instructions from https://support.nordvpn.com/Connectivity/Linux/1325531132/Installing-and-using-NordVPN-on-Debian-Ubuntu-and-Linux-Mint.htm
 
 wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-dpkg -i nordvpn-release_1.0.0_all.deb
+sudo dpkg -i nordvpn-release_1.0.0_all.deb
 
 sudo apt update
 sudo apt install nordvpn -y
